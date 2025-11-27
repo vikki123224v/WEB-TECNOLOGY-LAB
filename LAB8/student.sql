6758fc6aaff44f227020bd862abb3cc0768c9917
vikki123224v <vighneshwaras283@gmail.com>
2025-11-27 09:48:32 +0530

    Create student database and initial table with data

---
+++ LAB8/student.sql
@@ -0,0 +1,9 @@
+show databases;
+
+create database students128;
+use students128;
+create table students128 (id int primary key , name varchar(50) , grade float);
+
+insert into students128 values (1,"Vighneshwara",99);
+insert into students128 values (2,"Vaibhava",90);
+insert into students128 values (3,"Swaroop",95);
